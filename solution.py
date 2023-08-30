de90b088ea31bda61a49650b3cece026d676b098
Hoyoung Lee <sohostory@gmail.com>
2023-08-30 15:36:23 -0500

    added testing

--- solution.py
+++ solution.py
@@ -1,18 +1,27 @@
+import random
 from random import randint
 # you will need to run this on your machine and not this website for the sys.argv to work!
 import sys
 
-answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('you are a genius!')
+            return True
+    else:
+        print('hey bozo, I said 1~10')
+        return False
 
-while True:
-    try:
-        guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
-        if  0 < guess < 11:
-            if guess == answer:
-                print('you are a genius!')
+if __name__ == '__main__':
+    # answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+    answer = random.randint(1,10)
+
+    while True:
+        try:
+            guess = int(input(f'guess a number 1~10:  '))
+            if (run_guess(guess, answer)):
                 break
-        else:
-            print('hey bozo, I said 1~10')
-    except ValueError:
-        print('please enter a number')
-        continue
+
+        except ValueError:
+            print('please enter a number')
+            continue
